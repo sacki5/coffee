1a3d6222a4f441d1c120e1c01b1437d1c7d707d7
Isac Ljung <Isacljung@gmail.com>
2017-05-03 15:07:00 +0200

    Changed import of ctamaran font

--- header.php
+++ header.php
@@ -6,38 +6,37 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		<link rel="stylesheet" href="font-awesome.min.css">
+		<link href="https://fonts.googleapis.com/css?family=Catamaran:500,700,800" rel="stylesheet">
 		<link rel="stylesheet" href="style.css">
 
 	</head>
 <body id="top">
-		<a href="#top">
-			<div id="scroll-up">
-				<i class="fa fa-arrow-up" aria-hidden="true"></i>
+	<a href="#top">
+		<div id="scroll-up">
+			<i class="fa fa-arrow-up" aria-hidden="true"></i>
+		</div>
+	</a>
+	<header  class="header">
+		<a href="/coffee/">
+			<div class="logo">
+				<img alt="Logotyp för coffee" src="img/logo.png" />
 			</div>
 		</a>
-	<header  class="header">
-			<a href="/coffee/">
-				<div class="logo">
-					<img alt="Logotyp för coffee" src="img/logo.png" />
-				</div>
-			</a>
-			<nav class="main-menu">
-				<ul class="menu">
-					<li>
-						<a href="/coffee/">Hem</a>
-					</li>
-					<li>
-						<a href="omoss">Om oss</a>
-					</li>
-					<li>
-						<a href="kaffe">Vårt kaffe</a>
-					</li>
-					<li>
-						<a href="plugga">Plugga/arbeta på Coffee</a>
-					</li>
-
-				</ul>
-				<i id="menu-toggle" class="fa fa-bars" aria-hidden="true"></i>
-			</nav>
-
+		<nav class="main-menu">
+			<ul class="menu">
+				<li>
+					<a href="/coffee/">Hem</a>
+				</li>
+				<li>
+					<a href="omoss">Om oss</a>
+				</li>
+				<li>
+					<a href="kaffe">Vårt kaffe</a>
+				</li>
+				<li>
+					<a href="plugga">Plugga/arbeta på Coffee</a>
+				</li>
+			</ul>
+			<i id="menu-toggle" class="fa fa-bars" aria-hidden="true"></i>
+		</nav>
 	</header>
