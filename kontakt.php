90ee7c828e87117050df012464a979a5ffbdbcb9
Isac Ljung <Isacljung@gmail.com>
2017-05-11 15:46:18 +0200

    fef

--- kontakt.php
+++ kontakt.php
@@ -10,9 +10,27 @@
     <div class="row">
         <div class="col-sm-6">
             <h1>Kontakt uppgifter</h1>
-            <p>Bagers plats 1, 211 18 Malmö</p>
-            <p>070 123 45 67</p>
-            <p>coffee@gmail.com</p>
+            <p>Bagers plats 1, 211 18 Malmö<br />
+            <a href="tel:0701234567">070 123 45 67</a><br />
+            <a href="mailto:coffee@gmail.com">coffee@gmail.com</a></p><br />
+            <div id="map"></div>
+            <script type="text/javascript">
+            function initMap() {
+                var myLatLng = {lat: 55.6087077, lng: 12.9960739};
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 13,
+                    center: myLatLng,
+                    disableDefaultUI: true
+                });
+
+                var marker = new google.maps.Marker({
+                  position: myLatLng,
+                  map: map,
+                  title: 'Hello World!'
+                });
+            }
+            </script>
+            <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAciCS1aAGBmQKIUzMlpV8OPK4Nt58OyCM&callback=initMap"></script>
         </div>
         <div class="col-sm-6">
             <h1>Kontakt formulär</h1>
@@ -20,15 +38,16 @@
 
                 <div class="form-group has-feedback">
                     <label class="control-label">Namn</label>
-                    <input type="text" class="form-control" required>
+                    <input type="text" class="form-control" data-error="Detta fält är obligatoriskt" required>
                     <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
-
+                    <div class="help-block with-errors"></div>
                 </div>
 
                 <div class="form-group has-feedback">
                     <label class="control-label">Epost</label>
-                    <input type="email" class="form-control" required>
+                    <input type="email" class="form-control" data-error="Du måste fylla i en giltlig epost" required>
                     <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                    <div class="help-block with-errors"></div>
                 </div>
 
                 <div class="form-group has-feedback">
@@ -40,8 +59,9 @@
 
                 <div class="form-group has-feedback">
                     <label class="control-label">Meddelande</label>
-                    <textarea type="text" class="form-control" rows="4" required></textarea>
+                    <textarea type="text" class="form-control" rows="4" data-error="Detta fält är obligatoriskt" required></textarea>
                     <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                    <div class="help-block with-errors"></div>
                 </div>
 
                 <button type="submit" class="btn btn-custom">Submit</button>
